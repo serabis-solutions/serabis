2c78c03c84535bcda51228aa59d901fe1a319741
Graeme Lawton <graeme.lawton@setfiremedia.com>
2016-02-27 10:57:38 +0000

    Added agents tables, tidied up some psql stuff.

--- models/database.js
+++ models/database.js
@@ -3,7 +3,7 @@ var config;
 
 class Database {
     constructor() {
-        this.pg = require('pg');
+        this.pg = require('pg').native;
         this.db = new this.pg.Client('postgres://serapis:reallysecure@localhost:5432/serapis_dev');
         this.db.connect();
 
@@ -12,16 +12,29 @@ class Database {
         }
     }
 
-    saveDataPoints(dataPoints, agentId) {
-        var i;
-        for (i = 0; i < dataPoints.length; i++) {
-            var q = this.db.query(
-                "INSERT INTO data_points (data, agent_id) VALUES ($1, $2)", 
-                [dataPoints[i], agentId]
-            );
-        }
 
-        return { dataPointsSaved: i };
+    _buildDataPointsQuery(dataPoints, agentId) {
+        var params = []
+        var chunks = []
+        for(var i = 0; i < dataPoints.length; i++) {
+            var dataPoint = dataPoints[i]
+            var valueClause = []
+            params.push(dataPoint)
+            valueClause.push('$' + params.length)
+            params.push(agentId)
+            valueClause.push('$' + params.length)
+            chunks.push('(' + valueClause.join(', ') + ')')
+         }
+
+        return {
+            text: 'INSERT INTO data_points (data, agent_key) VALUES ' + chunks.join(', '),
+            values: params
+        } 
+    }
+
+    saveDataPoints(dataPoints, agentId) {
+        var q = this.db.query(this._buildDataPointsQuery(dataPoints, agentId) );
+        return { dataPointsSaved: dataPoints.length };
     }
 }
 
