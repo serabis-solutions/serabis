c8433968700d46db405396a5ea4b6a4875e4fa37
Mark Ellis <m@rkellis.com>
2016-04-17 14:56:27 +0100

    updated readme for first run

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -3,6 +3,7 @@
 install node
 install postgres
 
+npm install -g db-migrate
 if [[ ! $(sudo -u postgres psql -t -c "SELECT datname FROM pg_database WHERE datname = 'serapis_dev'") ]]; then 
     sudo -u postgres psql -c "CREATE USER serapis WITH PASSWORD 'reallysecure';"
     sudo -u postgres createdb serapis_dev -O serapis
