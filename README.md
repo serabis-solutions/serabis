e9d7616961278771292135f5946f13f48ad94ec3
Mark Ellis <m@rkellis.com>
2016-07-18 10:33:08 +0100

    re #56 - vagrant - serabis rename

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Serapis
+# Serabis
 
 First things first run vagrant up, then follow steps in api/README.md to setup api.
 
@@ -6,7 +6,7 @@ From the steps in api/README.md you should be able to generate an agent and acco
 
 Then follow the steps in agent/README.md to add toml config and setup the agent.
 
-Then navigate to http://dev.serapis/#/?agent={YOUR_AGENT_KEY}
+Then navigate to http://dev.serabis/#/?agent={YOUR_AGENT_KEY}
 
 If it all works you might have a graph!
 
--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -4,26 +4,26 @@ install node
 install postgres
 
 npm install -g db-migrate
-if [[ ! $(sudo -u postgres psql -t -c "SELECT datname FROM pg_database WHERE datname = 'serapis_dev'") ]]; then 
-    sudo -u postgres psql -c "CREATE USER serapis WITH PASSWORD 'reallysecure';"
-    sudo -u postgres createdb serapis_dev -O serapis
+if [[ ! $(sudo -u postgres psql -t -c "SELECT datname FROM pg_database WHERE datname = 'serabis_dev'") ]]; then 
+    sudo -u postgres psql -c "CREATE USER serabis WITH PASSWORD 'reallysecure';"
+    sudo -u postgres createdb serabis_dev -O serabis
 fi
 
 if [ ! -e /sub/sbin/apache2 ]; then
     sudo apt-get -y install apache2
-    sudo cp /vagrant/vagrant/conf/serapis-frontend.conf /etc/apache2/sites-enabled/
+    sudo cp /vagrant/vagrant/conf/serabis-frontend.conf /etc/apache2/sites-enabled/
     sudo a2enmod headers
     sudo systemctl reload apache2
 fi
 
 install rabbitmq
 
-sudo rabbitmqctl list_vhosts | grep "^serapis$" -q || {
-    sudo rabbitmqctl add_vhost serapis
+sudo rabbitmqctl list_vhosts | grep "^serabis$" -q || {
+    sudo rabbitmqctl add_vhost serabis
     #give admin use access to this vhost in the managment interface
-    sudo rabbitmqctl set_permissions -p serapis admin ".*" ".*" ".*"
+    sudo rabbitmqctl set_permissions -p serabis admin ".*" ".*" ".*"
 }
 sudo rabbitmqctl list_users | grep "^inserter\b" -q || {
     sudo rabbitmqctl add_user inserter reallysecure
-    sudo rabbitmqctl set_permissions -p serapis inserter ".*" ".*" ".*"
+    sudo rabbitmqctl set_permissions -p serabis inserter ".*" ".*" ".*"
 }
